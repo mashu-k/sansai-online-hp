11f879becc6eb8d87e512c5f35d6b0988eb54016
mashu-k <itazuramasyu@gmail.com>
2025-12-22 20:03:04 +0900

    faviconの設定 ga4同期

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Script from "next/script";
 import "../app/globals.css";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
@@ -12,6 +13,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ja" suppressHydrationWarning>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-TD5DFH0H0Q"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-TD5DFH0H0Q');
+            `,
+          }}
+        />
+      </head>
       <body>
         <ThemeProvider>
           <Navigation />
